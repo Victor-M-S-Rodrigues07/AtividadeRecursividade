f9f0a04e58ec53985cd2a5162013c2987aad5caf
Victor Moreira <victorvicmr@gmail.com>
2025-03-24 20:25:26 -0300

    Sequência de Fibonacci

--- Atividade de Recursividade.c
+++ Atividade de Recursividade.c
@@ -25,25 +25,58 @@
 
 // 2) Soma de Números Naturais
 
+// #include <stdio.h>
+
+// int soma (int n) {
+
+//     if (n <= 0) {
+
+//         return 0;
+//     }
+
+//     else {
+
+//         return n + soma (n - 1);
+//     }
+// }
+
+// int main () {
+
+//     int n = 7;
+
+//     printf ("Soma dos primeiros %d numeros: %d\n", n, soma (n));
+
+// }
+
+// 3) Sequência de Fibonacci
+
 #include <stdio.h>
 
-int soma (int n) {
+int a = 0;
+int b = 1;
 
-    if (n <= 0) {
+int fibonacci (int n) {
+
+    if (n == 0) {
 
         return 0;
     }
 
+    else if (n == 1) {
+
+        return 1;
+    }
+
     else {
 
-        return n + soma (n - 1);
+        return fibonacci (n - 1) + fibonacci (n - 2);
     }
+
 }
 
 int main () {
 
-    int n = 7;
-
-    printf ("Soma dos primeiros %d numeros: %d\n", n, soma (n));
-
+    int n = 25;
+        
+    printf ("Enesimo termo da sequencia de Fibonacci: %d", fibonacci (n));
 }

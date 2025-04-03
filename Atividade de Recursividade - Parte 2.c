1e5f0fde53c5edfdc01f5c258b41f02bfebea8ec
Victor Moreira <victorvicmr@gmail.com>
2025-04-03 12:39:41 -0300

    Verificação de Número Primo

--- Atividade de Recursividade - Parte 2.c
+++ Atividade de Recursividade - Parte 2.c
@@ -35,28 +35,83 @@
 
 // 12) Soma de Elementos de um Array
 
+// #include <stdio.h>
+// #include <stdlib.h>
+
+// int somaElementosArray (int array[], int tamanho, int indice) {
+
+//     if (indice >= tamanho) {
+
+//         return 0;
+//     }
+
+//     else {
+
+//         return array [indice] + somaElementosArray (array, tamanho, indice + 1);
+//     }
+// }
+
+// int main () {
+
+//     int array[] = {1, 2, 3, 4, 5};
+//     size_t tamanho = sizeof(array) / sizeof (array[0]); 
+    
+//     //Nota mental: O retorno de um "sizeof" é size_t, ou seja, foi recomendado usar um tipo diferente de dados;
+
+//     printf ("A soma dos elementos da array: %d", somaElementosArray (array, tamanho, 0));
+// }
+
+// 13) Verificação de Número Primo
+
 #include <stdio.h>
-#include <stdlib.h>
 
-int somaElementosArray (int array[], int tamanho, int indice) {
+int verificarPrimo (int N, int divisor) {
 
-    if (indice >= tamanho) {
+    if (N <= 1) {
 
         return 0;
     }
 
+    else if (N == 2) {
+
+        return 1;
+    }
+
     else {
 
-        return array [indice] + somaElementosArray (array, tamanho, indice + 1);
+        if (N % divisor == 0) {
+
+            return 0;
+        }
+
+        else {
+
+            if (divisor * divisor > N) {
+
+                return 1;
+            }
+
+            else {
+
+                return verificarPrimo (N, divisor + 1);
+            }
+        }
     }
 }
 
 int main () {
 
-    int array[] = {1, 2, 3, 4, 5};
-    size_t tamanho = sizeof(array) / sizeof (array[0]); 
-    
-    //Nota mental: O retorno de um "sizeof" é size_t, ou seja, foi recomendado usar um tipo diferente de dados;
+    int N = 64;
 
-    printf ("A soma dos elementos da array: %d", somaElementosArray (array, tamanho, 0));
+    int resultado = verificarPrimo (N, 2);
+
+    if (resultado == 0) {
+
+        printf ("O numero nao e primo");
+    }
+
+    else {
+
+        printf ("O numero e primo!");
+    }
 }

5145a3970b85d0fd804d79fb07a5a56f582e0e14
Victor Moreira <victorvicmr@gmail.com>
2025-04-04 16:53:56 -0300

    Comprimento de uma String

--- Atividade de Recursividade - Parte 2.c
+++ Atividade de Recursividade - Parte 2.c
@@ -241,43 +241,69 @@
 
 // 17) Busca em Array
 
+// #include <stdio.h>
+
+// int verificarArray (int N, int array[], int indice, int tamanho) {
+
+//     if (indice > tamanho) {
+
+//         return 0;
+//     }
+
+//     else {
+
+//         if (array[indice] == N) {
+
+//             return 1;
+//         }
+
+//         else {
+
+//             return verificarArray (N, array, indice + 1, tamanho);
+//         }
+//     }
+// }
+
+// int main () {
+
+//     int N = 10;
+//     int array[] = {5, 8, 12, 3, 7, 1, 9, 0, 4, 6};
+//     size_t tamanho = sizeof (array) / sizeof (array[0]);  
+//     int resultado = verificarArray (N, array, 0, tamanho);
+
+//     if (resultado == 0) {
+
+//         printf ("O numero %d nao foi encontrado na array", N);
+//     }
+
+//     else {
+
+//         printf ("O numero %d foi encontrado na array!", N);
+//     }
+// }
+
+// 18) Comprimento de uma String
+
+//OBS: Não pode usar "strlen"
+
 #include <stdio.h>
 
-int verificarArray (int N, int array[], int indice, int tamanho) {
+int comprimentoString (char palavra[], int indice, int contador) {
 
-    if (indice > tamanho) {
+    if (palavra[indice] == '\0') {
 
-        return 0;
+        return contador;
     }
 
     else {
 
-        if (array[indice] == N) {
-
-            return 1;
-        }
-
-        else {
-
-            return verificarArray (N, array, indice + 1, tamanho);
-        }
+        return comprimentoString (palavra, indice + 1, contador + 1);
     }
 }
 
 int main () {
 
-    int N = 10;
-    int array[] = {5, 8, 12, 3, 7, 1, 9, 0, 4, 6};
-    size_t tamanho = sizeof (array) / sizeof (array[0]);  
-    int resultado = verificarArray (N, array, 0, tamanho);
+    char palavra[] = "Maca";
 
-    if (resultado == 0) {
-
-        printf ("O numero %d nao foi encontrado na array", N);
-    }
-
-    else {
-
-        printf ("O numero %d foi encontrado na array!", N);
-    }
+    printf ("A palavra %s tem %d letras.", palavra, comprimentoString (palavra, 0, 0));
 }
